0abf4c05f04c69612e3c44a05b9309e2fe330259
Nelosh <asakura.jo@gmail.com>
2016-07-11 11:18:26 +0300

    GET all method added

--- src/main/java/lv/ctco/controllers/SessionController.java
+++ src/main/java/lv/ctco/controllers/SessionController.java
@@ -1,9 +1,27 @@
 package lv.ctco.controllers;
 
+import lv.ctco.entities.KnowledgeSession;
+import lv.ctco.repository.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/session")
 public class SessionController {
+
+    @Autowired
+    SessionRepository sessionRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> getAllSessions() {
+        List<KnowledgeSession> sessions = sessionRepository.findAll();
+        return new ResponseEntity<>(sessions, HttpStatus.OK);
+    }
 }
--- src/main/java/lv/ctco/repository/FeedbackRepository.java
+++ src/main/java/lv/ctco/repository/FeedbackRepository.java
@@ -1,7 +1,8 @@
 package lv.ctco.repository;
 
+import lv.ctco.entities.Feedback;
 import lv.ctco.entities.KnowledgeSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FeedbackRepository extends JpaRepository<KnowledgeSession,Long> {
+public interface FeedbackRepository extends JpaRepository<Feedback,Long> {
 }
