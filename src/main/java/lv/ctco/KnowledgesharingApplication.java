4802d59e6cc4e5934b7e778dee57b4e42654e791
Edwin <e.malisevskis@gmail.com>
2016-07-11 09:47:47 +0300

    broken commit

---
+++ src/main/java/lv/ctco/KnowledgesharingApplication.java
@@ -0,0 +1,12 @@
+package lv.ctco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgesharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgesharingApplication.class, args);
+	}
+}
