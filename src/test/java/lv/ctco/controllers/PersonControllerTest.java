cc44062db3b4d871a127209b3094ab162547c166
Edwin <e.malisevskis@gmail.com>
2016-07-11 16:24:57 +0300

    fixed #11, test for user is working

--- src/test/java/lv/ctco/controllers/PersonControllerTest.java
+++ src/test/java/lv/ctco/controllers/PersonControllerTest.java
@@ -1,6 +1,7 @@
 package lv.ctco.controllers;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.parsing.Parser;
 import lv.ctco.KnowledgeSharingApplication;
 import lv.ctco.entities.Person;
@@ -9,12 +10,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
-import static lv.ctco.Consts.*;
+import static lv.ctco.Consts.JSON;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = KnowledgeSharingApplication.class)
@@ -31,10 +34,13 @@ public class PersonControllerTest {
     @Test
     public void testGetByIDOK() {
         Person person = new Person();
-        person.setUserName("johnes123");
+        person.setUserName("notAName");
         person.setPassword("1234DDD");
+        person.setFullName("John Snow");
 
-        given().contentType(JSON).body(person).when().post("/register").then().statusCode(BAD_REQUEST);
+        Headers header = given().contentType(JSON).body(person).when().post("/register/").getHeaders();
+        get(header.getValue("Location")).then().body("password", equalTo("1234DDD"));
+
 
     }
 }
