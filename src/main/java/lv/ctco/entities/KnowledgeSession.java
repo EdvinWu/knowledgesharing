222c61cf8d009658da413ffce77de4863575d339
Nelosh <asakura.jo@gmail.com>
2016-07-11 10:37:36 +0300

    session added

---
+++ src/main/java/lv/ctco/entities/KnowledgeSession.java
@@ -0,0 +1,68 @@
+package lv.ctco.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+public class KnowledgeSession {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String title;
+    private String author;
+    private List<String> tags;
+    private List<Feedback> feedbacks;
+    private List<Person> users;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public List<Feedback> getFeedbacks() {
+        return feedbacks;
+    }
+
+    public void setFeedbacks(List<Feedback> feedbacks) {
+        this.feedbacks = feedbacks;
+    }
+
+    public List<Person> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<Person> users) {
+        this.users = users;
+    }
+}
