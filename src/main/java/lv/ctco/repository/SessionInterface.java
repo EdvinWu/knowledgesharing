a20a94dbbc19b7237ccc7f68492bddbc1aa8dc8f
Edwin <e.malisevskis@gmail.com>
2016-07-11 10:32:13 +0300

    added users and session repos

---
+++ src/main/java/lv/ctco/repository/SessionInterface.java
@@ -0,0 +1,6 @@
+package lv.ctco.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionInterface extends JpaRepository<Session,Long> {
+}
---
+++ src/main/java/lv/ctco/repository/UsersRepository.java
@@ -0,0 +1,6 @@
+package lv.ctco.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<User,Long>{
+}
