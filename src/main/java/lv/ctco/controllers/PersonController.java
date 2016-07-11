0cdf2e6b49c2d347a89d29117b80739883f06267
Edwin <e.malisevskis@gmail.com>
2016-07-11 12:21:02 +0300

    fixed #5, controller implemented

--- src/main/java/lv/ctco/controllers/PersonController.java
+++ src/main/java/lv/ctco/controllers/PersonController.java
@@ -1,13 +1,14 @@
 package lv.ctco.controllers;
 
+import lv.ctco.entities.Person;
 import lv.ctco.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -18,9 +19,12 @@ public class PersonController {
 
 
     @RequestMapping(path = "register",method = RequestMethod.POST)
-    public ResponseEntity<?> registerPerson() {
-
+    public ResponseEntity<?> registerPerson(@RequestBody Person person) {
+    if (personRepository.exists(person.getId())){
+        personRepository.save(person);
         return new ResponseEntity<>(HttpStatus.OK);
+    }else
+        return  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     @RequestMapping(path = "person/{id}",method = RequestMethod.GET)
@@ -36,20 +40,32 @@ public class PersonController {
 
     @RequestMapping(path = "person/",method = RequestMethod.GET)
     public ResponseEntity<?> getAllPersons() {
-
-        return new ResponseEntity<>(HttpStatus.OK);
+    List<Person> personList = personRepository.findAll();
+        return new ResponseEntity<>(personList,HttpStatus.OK);
     }
 
+    @Transactional
     @RequestMapping(path = "person/{id}",method = RequestMethod.DELETE)
-    public ResponseEntity<?> deletePersonById() {
-
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<?> deletePersonById(@PathVariable("id") long id) {
+        if (personRepository.exists(id)) {
+            personRepository.delete(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @RequestMapping(path = "person/{id}",method = RequestMethod.PUT)
-    public ResponseEntity<?> updateUser() {
+    @Transactional
+    @RequestMapping(path = " /{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateStudentByID(@PathVariable("id") long id,
+                                               @RequestBody Person person) {
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        if (personRepository.exists(id)) {
+            Person editedPerson = personRepository.findOne(id);
+            editedPerson.setFullName(person.getFullName());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 
