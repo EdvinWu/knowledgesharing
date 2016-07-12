985fa4f4fa172e452b2dd39f963194dc9f6e1624
N Okunev <nokunevs@gmail.com>
2016-07-12 10:45:15 +0300

    #31 implemented sessionDateController

---
+++ src/main/java/lv/ctco/controllers/DateSessionController.java
@@ -0,0 +1,49 @@
+package lv.ctco.controllers;
+
+import lv.ctco.repository.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+import static lv.ctco.Consts.SESSION_PATH;
+
+
+@RestController
+@RequestMapping
+public class DateSessionController {
+
+    @Autowired
+    SessionRepository sessionRepository;
+
+    @RequestMapping(path = "/{id}" + SESSION_PATH + "/{fId}", method = RequestMethod.POST)
+    public ResponseEntity<?> addDate(@PathVariable("id") long id) {
+            Date date = new Date();
+        sessionRepository.findOne(id).addDate(date);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @RequestMapping(path = "/{id}" + SESSION_PATH + "/{fId}", method = RequestMethod.GET)
+    public ResponseEntity<?> addDate(@PathVariable("id") long id) {
+        sessionRepository.findOne(id).getDate();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/{id}" + SESSION_PATH + "/{fId}", method = RequestMethod.PUT)
+    public ResponseEntity<?> addDate(@PathVariable("id") long id, Date date) {
+        sessionRepository.findOne(id).setDate(date);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @RequestMapping(path = "/{id}" + SESSION_PATH + "/{fId}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> addDate(@PathVariable("id") long id) {
+        sessionRepository.findOne(id).setDate(null);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
