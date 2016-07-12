50ef1a55b17fc8e201ab0e9acc06a602904d37c3
N Okunev <nokunevs@gmail.com>
2016-07-12 10:55:56 +0300

    fixed #31, added if/else statements

--- src/main/java/lv/ctco/controllers/DateSessionController.java
+++ src/main/java/lv/ctco/controllers/DateSessionController.java
@@ -23,27 +23,39 @@ public class DateSessionController {
 
     @RequestMapping(path = "/{id}" + SESSION_PATH + "/{fId}", method = RequestMethod.POST)
     public ResponseEntity<?> addDate(@PathVariable("id") long id) {
+        if (sessionRepository.exists(id)){
             Date date = new Date();
-        sessionRepository.findOne(id).addDate(date);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+            sessionRepository.findOne(id).addDate(date);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @RequestMapping(path = "/{id}" + SESSION_PATH + "/{fId}", method = RequestMethod.GET)
     public ResponseEntity<?> addDate(@PathVariable("id") long id) {
-        sessionRepository.findOne(id).getDate();
-        return new ResponseEntity<>(HttpStatus.OK);
+        if (sessionRepository.exists(id)){
+            sessionRepository.findOne(id).getDate();
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @RequestMapping(path = "/{id}" + SESSION_PATH + "/{fId}", method = RequestMethod.PUT)
     public ResponseEntity<?> addDate(@PathVariable("id") long id, Date date) {
-        sessionRepository.findOne(id).setDate(date);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        if (sessionRepository.exists(id)){
+            sessionRepository.findOne(id).setDate(date);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @RequestMapping(path = "/{id}" + SESSION_PATH + "/{fId}", method = RequestMethod.DELETE)
     public ResponseEntity<?> addDate(@PathVariable("id") long id) {
-        sessionRepository.findOne(id).setDate(null);
-        return new ResponseEntity<>(HttpStatus.OK);
+        if (sessionRepository.exists(id)){
+            sessionRepository.findOne(id).setDate(null);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
