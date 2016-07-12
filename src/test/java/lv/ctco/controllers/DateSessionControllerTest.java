e88699ae13d3e1c05b841ed9115736afed4f5c54
N Okunev <nokunevs@gmail.com>
2016-07-12 12:21:14 +0300

    date&time broken

--- src/test/java/lv/ctco/controllers/DateSessionControllerTest.java
+++ src/test/java/lv/ctco/controllers/DateSessionControllerTest.java
@@ -1,8 +1,10 @@
 package lv.ctco.controllers;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.parsing.Parser;
 import lv.ctco.KnowledgeSharingApplication;
+import lv.ctco.entities.KnowledgeSession;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +15,8 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.time.LocalDateTime;
 
+import static io.restassured.RestAssured.delete;
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static lv.ctco.Consts.*;
 import static org.junit.Assert.*;
@@ -24,6 +28,8 @@ import static org.junit.Assert.*;
 
 public class DateSessionControllerTest {
 
+
+
     @Before
     public void before() {
         RestAssured.port = 8090;
@@ -33,23 +39,35 @@ public class DateSessionControllerTest {
 
     @Test
     public void testAddDate() throws Exception {
+        KnowledgeSession kSession = new KnowledgeSession();
         LocalDateTime date = LocalDateTime.now();
-        given().contentType(JSON).body(date).when().post(DATE_PATH).then().statusCode(CREATED);
-        given().contentType(JSON).body(date).when().post(DATE_PATH).then().statusCode(NOT_FOUND);
+      Headers headers =  given()
+              .contentType(JSON)
+              .body(kSession)
+              .when()
+              .post(SESSION_PATH)
+              .getHeaders();
+
+        given().contentType(JSON).body(date).when().post(headers+DATE_PATH).then().statusCode(CREATED);
     }
 
     @Test
     public void testGetDate() throws Exception {
-
+        get(DATE_PATH).then().statusCode(OK);
     }
 
     @Test
     public void testUpdateDate() throws Exception {
-
+        LocalDateTime date = LocalDateTime.now();
+        given().contentType(JSON).body(date).when().put(DATE_PATH).then().statusCode(CREATED);
     }
 
     @Test
     public void testDeleteDate() throws Exception {
+        LocalDateTime date = LocalDateTime.now();
+        Headers header = given().contentType(JSON).body(date).when().post(REGISTER_PATH).getHeaders();
+        delete(header.getValue("Location")).then().statusCode(OK);
+
 
     }
 }
