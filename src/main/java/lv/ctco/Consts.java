1eb20cc26561f9731585d70b3529b29416fd684e
N Okunev <nokunevs@gmail.com>
2016-07-12 11:23:30 +0300

    Fixed #35 updated const class

--- src/main/java/lv/ctco/Consts.java
+++ src/main/java/lv/ctco/Consts.java
@@ -6,12 +6,12 @@ public class Consts {
     public static final String SESSION_PATH = "/sessions";
     public static final String FEEDBACK_PATH = "/feedback";
     public static final String JSON = "application/json";
-    public static final String SCHEDULED_PATH = "/scheduled";
     public static final String FALLING_ID = "/-1";
     public static final String REGISTER_PATH = "/register/";
     public static final int OK = HttpStatus.OK.value();
     public static final int NOT_FOUND = HttpStatus.NOT_FOUND.value();
     public static final int CREATED = HttpStatus.CREATED.value();
     public static final int BAD_REQUEST = HttpStatus.BAD_REQUEST.value();
+    public static final String DATE_PATH ="/date";
 
 }
---
+++ src/test/java/lv/ctco/controllers/DateSessionControllerTest.java
@@ -0,0 +1,54 @@
+package lv.ctco.controllers;
+
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import lv.ctco.KnowledgeSharingApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static io.restassured.RestAssured.given;
+import static lv.ctco.Consts.*;
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = KnowledgeSharingApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:8090")
+
+public class DateSessionControllerTest {
+
+    @Before
+    public void before() {
+        RestAssured.port = 8090;
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+
+    @Test
+    public void testAddDate() throws Exception {
+        
+
+        given().contentType(JSON).body(date).when().post(REGISTER_PATH).then().statusCode(CREATED);
+        given().contentType(JSON).body(person).when().post(REGISTER_PATH).then().statusCode(BAD_REQUEST);
+    }
+
+    @Test
+    public void testGetDate() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateDate() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteDate() throws Exception {
+
+    }
+}
