e891c43a679ff787d17408730cb9e2f2fffdaf71
N Okunev <nokunevs@gmail.com>
2016-07-13 09:48:19 +0300

    remove security

---
+++ src/main/java/lv/ctco/controllers/WebConfiguration.java
@@ -0,0 +1,14 @@
+package lv.ctco.controllers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+    @Bean
+    ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
