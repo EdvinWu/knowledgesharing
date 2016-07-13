875eae4a9b4dd65cefab2dd980193cc52db14200
N Okunev <nokunevs@gmail.com>
2016-07-13 09:52:24 +0300

    add console

--- src/main/java/lv/ctco/controllers/WebConfiguration.java
+++ src/main/java/lv/ctco/controllers/WebConfiguration.java
@@ -1,5 +1,7 @@
 package lv.ctco.controllers;
 
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
