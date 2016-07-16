e81cf4aef0465caefb4a8a0c366602cd8a0604cf
Alina Vasilevska <vasilek93@inbox.lv>
2016-07-16 17:17:36 +0300

    changed  back security that i have commented for easier manual testing

--- src/main/java/lv/ctco/configuration/SecurityConfiguration.java
+++ src/main/java/lv/ctco/configuration/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers("/students/**").authenticated().and().formLogin()
+                .antMatchers("/sessions/**").authenticated().and().formLogin()
                 .loginPage("/login.html")
                 .permitAll().and()
                 .httpBasic().and()
