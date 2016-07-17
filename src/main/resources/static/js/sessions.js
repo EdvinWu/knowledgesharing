f14c65f8c83aba2a6f5f4d8db3eaf0188d6ab0fb
Alina Vasilevska <vasilek93@inbox.lv>
2016-07-17 18:21:09 +0300

    now it is possible to view session details, but fixes in html are still needed

--- src/main/resources/static/js/sessions.js
+++ src/main/resources/static/js/sessions.js
@@ -1,7 +1,7 @@
 var currentSessionView = "all";
 
 function loadSessions(status) {
-        
+
     if (status === undefined) {
         status = currentSessionView;
     } else {
@@ -10,7 +10,7 @@ function loadSessions(status) {
 
     return $.ajax({
         type: 'GET',
-        url: 'http://localhost:8080/sessions/bystatus/'+ status,
+        url: 'http://localhost:8080/sessions/bystatus/' + status,
         dataType: 'json',
         statusCode: {
             200: function (data) {
@@ -21,17 +21,17 @@ function loadSessions(status) {
 }
 
 function drawSessionList(status) {
-    loadSessions(status).then(function(sessions) {
+    loadSessions(status).then(function (sessions) {
         var sessionList = document.querySelector(".session-list");
         if (sessionList) {
             document.body.removeChild(sessionList.parentNode);
         }
-        
+
         var sessionListTemplate = Handlebars.compile(document.querySelector('#session-list').innerHTML);
         var sessionTemplate = Handlebars.compile(document.querySelector('#session').innerHTML);
 
         var sessionList = '';
-        sessions.forEach(function(session) {
+        sessions.forEach(function (session) {
             sessionList += sessionTemplate(session);
         });
 
@@ -40,8 +40,7 @@ function drawSessionList(status) {
         });
 
         var listTitle;
-        switch(currentSessionView)
-        {
+        switch (currentSessionView) {
             case "done":
                 listTitle = "Done Sessions";
                 break;
@@ -56,7 +55,7 @@ function drawSessionList(status) {
                 break;
         }
         var elListTitle = document.getElementById("sessionListName");
-        if(elListTitle){
+        if (elListTitle) {
             elListTitle.innerHTML = listTitle;
         }
 
@@ -67,14 +66,14 @@ function drawSessionList(status) {
     });
 }
 
-function addSession(event){
+function addSession(event) {
     $.ajax({
         type: 'POST',
         url: 'http://localhost:8080/sessions',
         data: JSON.stringify({
-            title:document.session.title.value,
-            author:document.session.host.value,
-            description:document.session.description.value
+            title: document.session.title.value,
+            author: document.session.host.value,
+            description: document.session.description.value
         }),
         contentType: 'application/json',
         dataType: 'json',
@@ -92,10 +91,10 @@ function addSession(event){
     window.location = "http://localhost:8080/index.html";
 }
 
-function changeSessionStatus(event, id, status){
+function changeSessionStatus(event, id, status) {
     $.ajax({
         type: 'PUT',
-        url: 'http://localhost:8080/sessions/' + id + '/changestatus/'  + status,
+        url: 'http://localhost:8080/sessions/' + id + '/changestatus/' + status,
         statusCode: {
             200: function (data) {
                 drawSessionList();
@@ -107,7 +106,7 @@ function changeSessionStatus(event, id, status){
 
 }
 
-function removeElement(event, id){
+function removeElement(event, id) {
     $.ajax({
         type: 'DELETE',
         url: 'http://localhost:8080/sessions/' + id,
@@ -122,17 +121,37 @@ function removeElement(event, id){
     event.preventDefault();
 }
 
-function exploreElement(event, id){
-    window.location = "http://localhost:8080/sessionDetails.html/";
-    $.ajax({
+function loadSessionById(id) {
+    return $.ajax({
         type: 'GET',
         url: 'http://localhost:8080/sessions/' + id,
         statusCode: {
             200: function (data) {
-                drawSessionList();
                 return data;
             }
         }
     });
     event.preventDefault();
+}
+
+function exploreElement(event, id) {
+    loadSessionById(id).then(function (sessions) {
+
+        var sessionListTemplate = Handlebars.compile(document.querySelector('#session-list').innerHTML);
+        var sessionTemplate = Handlebars.compile(document.querySelector('#session').innerHTML);
+
+        var sessionList = '';
+
+        sessionList += sessionTemplate(sessions);
+
+        var sessionList = sessionListTemplate({
+            body: sessionList
+        });
+
+        var sessionListContainer = document.createElement('div');
+        sessionListContainer.innerHTML = sessionList;
+        document.body.appendChild(sessionListContainer);
+
+        event.preventDefault();
+    });
 }
