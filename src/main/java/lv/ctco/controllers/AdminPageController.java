b9791951a5bca06740e5d6ecb74a0ffdf864353a
N Okunev <nokunevs@gmail.com>
2016-07-18 10:26:02 +0300

    merge with master

--- src/main/java/lv/ctco/controllers/AdminPageController.java
+++ src/main/java/lv/ctco/controllers/AdminPageController.java
@@ -1,21 +1,25 @@
 package lv.ctco.controllers;
 
-import lv.ctco.services.PersonService;
+import lv.ctco.entities.Person;
+import lv.ctco.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 public class AdminPageController {
     @Autowired
-    PersonService personService;
+    PersonRepository personRepository;
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = {"/adminPage"})
-    public String index(Model model) {
-        model.addAttribute("user", personService.getAuthenticatedPerson());
+    public String index(Model model, Principal principal) {
+        Person loggedPerson = personRepository.findUserByLogin(principal.getName());
+        model.addAttribute("user", loggedPerson);
         return "adminPage";
     }
 }
